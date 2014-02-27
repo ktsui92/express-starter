1cd8129bd7bebe23cdf4ca06c715195639f9ba98
Krystal Tsui <k.tsui92@gmail.com>
2014-02-26 19:03:52 -0800

    Completed Animals problem!

--- static/js/imgrr.js
+++ static/js/imgrr.js
@@ -1,7 +1,13 @@
 // Problem 2 (Peekaboo) ------------------------------------------------------
-$('#toggleimg').click(function() {
-$('#main_img').hide()
-$('#toggleimg').text('Come Back!')
+$('#toggle_img').click(function() {
+if ('#toggle_img' === "Go Away!") {
+	console.log($('#main_img').hide()
+$('#toggle_img').text('Come Back!'));
+} else if ('#toggle_img' === "Come Back!") {
+	console.log(($('#main_img').show()
+$('#toggle_img').text('Go Away!'));
+}
+
 });
 
 // Problem 3 (Swap Em) -----------------------------------------------
