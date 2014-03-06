98dc734d2b11361a90c1c0600226f274b4fbad63
Krystal Tsui <k.tsui92@gmail.com>
2014-03-06 15:45:03 -0800

    Colors!

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -8,12 +8,10 @@ $(document).ready(function() {
   });
 
 $('#p1').click(function() {
-	context.strokeStyle = 'orange';
 	context.strokeRect(30,30,150,100);
 });
 
 $('#p2').click(function() {
-	context.strokeStyle = 'blue';
 	context.strokeRect(50,50,100,100);
 });
 
@@ -33,9 +31,26 @@ $('#p5').click(function(){
 	context.moveTo(250,250);
 	context.lineTo(80,100);
 	context.stroke();
-
 });
 
+$('#p6').click(function(){
+	context.strokeStyle = 'green';
+	context.strokeRect(70,90,150,100);	
+});
+
+$('#p7').click(function(){
+	context.beginPath();
+	context.arc(250,150,125,2,9);
+	context.fillStyle = 'red';
+	context.fill();
+});
+
+$('#p8').click(function(){
+	context.strokeStyle = 'blue';
+	context.strokeRect(50,50,175,80);
+	context.fillStyle = 'yellow';
+	context.fillRect(50,50,175,80);
+});
 
   //---------------------------------------------------------------------------
   //Write your code for p1-p12 here
