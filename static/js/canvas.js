6779377c23580557959aefdf259f76b43440ec11
Krystal Tsui <k.tsui92@gmail.com>
2014-03-05 18:45:53 -0800

    Mastered right angles!

---
+++ static/js/canvas.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  //this is how we acquire control of the canvas
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext("2d");
+
+  $('#clear').click(function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  });
+
+  //---------------------------------------------------------------------------
+  //Write your code for p1-p12 here
+  //
+
+});
